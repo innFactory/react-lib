ee9ea7e585d6c43293eb5aa6133bf087019e96d2
spoeck <toni.spoeck@web.de>
2018-10-18 14:25:22 +0200

    fix build

--- src/react-lib.ts
+++ src/react-lib.ts
@@ -1,1 +1,6 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+export default class DummyClass {}
+
 // export { default as CalculationRow } from './mui-components/CalculationRow';
