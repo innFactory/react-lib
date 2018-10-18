074f6ef1ef463803cfcd583f4666a3f32b49dfea
spoeck <toni.spoeck@web.de>
2018-10-18 12:53:03 +0200

    rename lib2 to lib

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,13 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import camelCase from 'lodash.camelcase';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
 
 const pkg = require('./package.json')
 
-const libraryName = 'react-lib2'
+const libraryName = 'react-lib'
 
 export default {
   input: `src/${libraryName}.ts`,
---
+++ src/react-lib.ts
@@ -0,0 +1,1 @@
+// export { default as CalculationRow } from './mui-components/CalculationRow';
