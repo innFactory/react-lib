26ba0de4e56cf674f07007f6c37ccc7d41ced595
spoeck <toni.spoeck@web.de>
2019-01-17 18:15:05 +0100

    switch to newest versions

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ export { default as CalculationRow } from './mui-components/CalculationRow';
 export { default as MovingBackgroundImage } from './mui-components/MovingBackgroundImage';
 export { default as RadioCalcRows } from './mui-components/RadioCalcRows';
 export { default as StyledSlider } from './mui-components/StyledSlider';
-export { numberToString, parseQueryString } from './utils';
+export { isIE, numberToString, parseQueryString } from './utils';
 
