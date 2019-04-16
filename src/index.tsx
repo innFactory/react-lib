6f732f60a9ce769abc95f8317ef4b5cec4deeb12
Patrick Stadler <p.stadler@innfactory.de>
2019-04-16 09:33:22 +0200

    VR-577

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ export { default as CalculationRow } from './mui-components/CalculationRow';
 export { default as MovingBackgroundImage } from './mui-components/MovingBackgroundImage';
 export { default as RadioCalcRows } from './mui-components/RadioCalcRows';
 export { default as StyledSlider } from './mui-components/StyledSlider';
+export { default as LandingPageFactory } from './landingPageComponentFactory/ComponentFactory';
 export { isIE, numberToString, parseQueryString } from './utils';
 
